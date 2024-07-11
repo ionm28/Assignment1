b512c18c26f596a011b11027d7f9e917983126ec
ionm28 <ion.mihaila03@gmail.com>
2024-07-11 14:54:25 +0200

    Improved second function#2

--- django_project/django_project/users/tests/test_adapters2.py
+++ django_project/django_project/users/tests/test_adapters2.py
@@ -1,21 +1,37 @@
 import pytest
 from django.http import HttpRequest
 from django.conf import settings
-from django_project.users.adapters import AccountAdapter
+from django_project.users.adapters import SocialAccountAdapter
 
 @pytest.fixture
 def my_instance():
-    return AccountAdapter()
+    return SocialAccountAdapter()
 
 #second function
-def test_is_open_for_signup_registration_allowed(my_instance):
+def test_is_open_for_signup_registration_allowed_no_sociallogin(my_instance):
     settings.ACCOUNT_ALLOW_REGISTRATION = True
     request = HttpRequest()
-    result = my_instance.is_open_for_signup(request)
+    sociallogin = None
+    result = my_instance.is_open_for_signup(request, sociallogin)
     assert result is True
 
-def test_is_open_for_signup_registration_not_allowed(my_instance):
+def test_is_open_for_signup_registration_allowed_with_sociallogin(my_instance):
+    settings.ACCOUNT_ALLOW_REGISTRATION = True
+    request = HttpRequest()
+    sociallogin = SocialAccountAdapter() 
+    result = my_instance.is_open_for_signup(request, sociallogin)
+    assert result is True
+
+def test_is_open_for_signup_registration_not_allowed_no_sociallogin(my_instance):
     settings.ACCOUNT_ALLOW_REGISTRATION = False
     request = HttpRequest()
-    result = my_instance.is_open_for_signup(request)
+    sociallogin = None  
+    result = my_instance.is_open_for_signup(request, sociallogin)
+    assert result is False
+
+def test_is_open_for_signup_registration_not_allowed_with_sociallogin(my_instance):
+    settings.ACCOUNT_ALLOW_REGISTRATION = False
+    request = HttpRequest()
+    sociallogin = SocialAccountAdapter()  
+    result = my_instance.is_open_for_signup(request, sociallogin)
     assert result is False
